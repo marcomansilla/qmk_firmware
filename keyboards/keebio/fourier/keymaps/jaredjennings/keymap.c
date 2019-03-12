6cba353852dacd6d4b39ecb59b0a49a5997c74eb
Jared Jennings <jjennings@fastmail.fm>
2019-03-12 12:56:16 -0400

    fix one shot Alt again

--- keyboards/keebio/fourier/keymaps/jaredjennings/keymap.c
+++ keyboards/keebio/fourier/keymaps/jaredjennings/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,   /**/    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS, KC_QUOT,
          KC_TAB,   KC_A,    KC_R,    KC_S,    KC_T,    KC_G,  /**/     KC_K,    KC_N,    KC_E,    KC_I,    KC_O,        KC_ENT,
          KC_LSPO,     KC_X,    KC_C,    KC_D,    KC_V,    KC_Z, /**/     KC_M,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
-         KC_LCHO, TT(KC_LALT), KC_LGUI, KC_NAV, KC_BSFN1,     /**/    KC_SPFN2,      TT(KC_RALT), KC_RGUI, KC_FN3,  KC_BSLS
+         KC_LCHO, OSM(MOD_LALT), KC_LGUI, KC_NAV, KC_BSFN1,     /**/    KC_SPFN2,      OSM(MOD_RALT), KC_RGUI, KC_FN3,  KC_BSLS
         ),
 
   [_QWERTY] =
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   /**/  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, KC_QUOT,
          KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  /**/   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,     KC_ENT,
          KC_LSPO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, /**/   KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
-         KC_LCHO, OSM(MOD_LALT), KC_LGUI, KC_NAV,  KC_BSFN1,   /**/       KC_SPFN2,                  KC_RALT, KC_RGUI, KC_FN3,  KC_BSLS
+         KC_LCHO, OSM(MOD_LALT), KC_LGUI, KC_NAV,  KC_BSFN1,   /**/       KC_SPFN2,                  OSM(MOD_RALT), KC_RGUI, KC_FN3,  KC_BSLS
          ),
 
   [_FN1] =

1d07e07b26d991053224471109c3341df07b2035
Jared Jennings <jjennings@fastmail.fm>
2020-04-01 14:30:44 -0400

    add Colemak DHm and make it the default, to ease use of my Fourier after using my Dactyl Manuform

--- keyboards/keebio/fourier/keymaps/jaredjennings/keymap.c
+++ keyboards/keebio/fourier/keymaps/jaredjennings/keymap.c
@@ -6,12 +6,13 @@ extern keymap_config_t keymap_config;
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _COLEMAK_DH_ANSI 0
-#define _QWERTY 1
-#define _FN1 2
-#define _FN2 3
-#define _FN3 4
-#define _NAV 5
+#define _COLEMAK_DHM 0
+#define _COLEMAK_DH_ANSI 1
+#define _QWERTY 2
+#define _FN1 3
+#define _FN2 4
+#define _FN3 5
+#define _NAV 6
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -35,6 +36,14 @@ enum custom_keycodes {
 #define KC_CT_R LCTL(KC_RIGHT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_COLEMAK_DHM] =
+  LAYOUT(
+         KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,   /**/    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS, KC_QUOT,
+         KC_TAB,   KC_A,    KC_R,    KC_S,    KC_T,    KC_G,  /**/     KC_M,    KC_N,    KC_E,    KC_I,    KC_O,        KC_ENT,
+         KC_LSPO,     KC_Z,    KC_X,    KC_C,    KC_D,    KC_V, /**/     KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
+         KC_LCHO, OSM(MOD_LALT), KC_LGUI, KC_NAV, KC_BSFN1,     /**/    KC_SPFN2,      OSM(MOD_RALT), KC_RGUI, KC_FN3,  KC_RCBS
+        ),
+
   [_COLEMAK_DH_ANSI] =
   LAYOUT(
          KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,   /**/    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS, KC_QUOT,
